2a4e0da64665821894970e13d7ee0c9c034af1aa
greguy06 <101117144+greguy06@users.noreply.github.com>
2022-03-10 15:08:56 +0100

    Linear interp changes 10/03

--- blendedwingbody.py
+++ blendedwingbody.py
@@ -13,15 +13,16 @@ DIR = os.path.dirname(__file__)
 class BlendedWingBody(GeomBase):
     n_section = Input(4)
     airfoils = Input(["whitcomb", "whitcomb", "whitcomb", "whitcomb", "whitcomb"])
-    s_chords = Input([6., 6., 6., 6., 6.])
-    t_factors = Input([1., 1., 1., 1., 1.])
+    s_chords = Input([20.0, 8.0, 5.0, 5.0, 5.0])
+    t_factors = Input([2.0, 1.0, 1.0, 1.0, 1.0])
 
-    s_spans = Input([1., 1., 1., 1.])
+    s_spans = Input([8.0, 13.0, 1.0, 3])
     sweep_locs = Input([0.2, 0.2, 0.2, 0.2])  # location of sweep
-    sweep_angles = Input([30, 30, 30, 30])  # sweep angle
-    dihedral_angles = Input([0, 0, 0, 0])
+    sweep_angles = Input([40, 50, 40, 40])  # sweep angle
+    dihedral_angles = Input([0, -10, 45, 45])
     twist_locs = Input([0.2, 0.2, 0.2, 0.2, 0.2])
-    twist_angles = Input([0, 0, 0, 0, 20])
+    twist_angles = Input([0, 0, 0, 0, 0])
+
 
     @Part
     def aircraft_frame(self):
@@ -34,12 +35,15 @@ class BlendedWingBody(GeomBase):
                                         "sweep_locs, sweep_angles, twist_locs, twist_angles"
                                         "dihedral_angles",
                               position=self.position,
+                              ruled=True,
+                              color='red',
                               mesh_deflection=0.0001)
 
     @Part
     def left_wing(self):
         return MirroredShape(shape_in=self.right_wing,
                              reference_point=self.position,
+                             color='red',
                              # Two vectors to define the mirror plane
                              vector1=self.position.Vz,
                              vector2=self.position.Vx,
--- liftingsurface.py
+++ liftingsurface.py
@@ -8,15 +8,15 @@ from ref_frame import Frame
 class LiftingSurface(LoftedSolid):  # note use of loftedSolid as superclass
     n_section = Input(4)
     airfoils = Input(["whitcomb", "whitcomb", "whitcomb", "whitcomb", "whitcomb"])
-    s_chords = Input([6., 6., 6., 6., 6.])
-    t_factors = Input([1., 1., 1., 1., 1.])
+    s_chords = Input([20.0, 8.0, 5.0, 5.0, 5.0])
+    t_factors = Input([2.0, 1.0, 1.0, 1.0, 1.0])
 
-    s_spans = Input([1., 1., 1., 1.])
+    s_spans = Input([8.0, 13.0, 1.0, 3])
     sweep_locs = Input([0.2, 0.2, 0.2, 0.2])  # location of sweep
-    sweep_angles = Input([30, 30, 30, 30])  # sweep angle
-    dihedral_angles = Input([0, 0, 0, 0])
+    sweep_angles = Input([40, 50, 40, 40])  # sweep angle
+    dihedral_angles = Input([0, -10, 45, 45])
     twist_locs = Input([0.2, 0.2, 0.2, 0.2, 0.2])
-    twist_angles = Input([0, 0, 0, 0, 20])
+    twist_angles = Input([0, 0, 0, 0, 0])
 
     @Attribute  # required input for the superclass LoftedSolid
     def profiles(self):
@@ -42,10 +42,10 @@ class LiftingSurface(LoftedSolid):  # note use of loftedSolid as superclass
                 lst_airfoils.append(A)
                 lst_positions.append(P)
             else:
-                P = rotate(translate(lst_positions[-1], 'x', self.sweep_locs[i-1]*self.s_chords[i-1] - (self.s_chords[i]  - (
+                P = rotate(rotate(translate(lst_positions[-1], 'x', self.sweep_locs[i-1]*self.s_chords[i-1] - (self.s_chords[i]  - (
                                        tan(radians(self.sweep_angles[i-1])) * self.s_spans[i-1] + (1-self.sweep_locs[i-1])*self.s_chords[i])), 'y', self.s_spans[i-1], 'z', self.s_spans[i-1] * tan(radians(self.dihedral_angles[i-1]))),
                Vector(self.twist_locs[i], 1, 0), self.twist_angles[i],
-               deg=True)
+               deg=True),Vector(self.twist_locs[i], 0, 0),self.dihedral_angles[i-1],deg=True)
                 A = Airfoil(airfoil_name=self.airfoils[i],
                        chord=self.s_chords[i],
                        thickness_factor=self.t_factors[i],
@@ -58,8 +58,9 @@ class LiftingSurface(LoftedSolid):  # note use of loftedSolid as superclass
 
     @Part
     def lofted_surf(self):
-        return LoftedSurface(profiles=self.profiles,
-                             hidden=not(__name__ == '__main__'))
+        return LoftedSolid(profiles=self.profiles,
+                           color='red',
+                           hidden=not(__name__ == '__main__'))
 
 
 if __name__ == '__main__':
